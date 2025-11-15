5f0f9c0960c3f1d0a4532125851372d688b31ea2
Dinotaurent <danieldazawow@gmail.com>
2025-11-15 17:16:39 -0500

    Fixes #4: Nota individual terminada

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,8 +1,10 @@
 import { getStatus } from "@actions/get-status.action";
 import { getNotas } from "@actions/notas/get-notas.action";
+import { getNotaById } from '@actions/notas/get-nota-by-id.action';
 
 
 export const server = {
     getStatus,
     getNotas,
+    getNotaById,
 };
