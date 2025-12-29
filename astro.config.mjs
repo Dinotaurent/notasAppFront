966af05362015ce688e8ef02a13fb6af43312074
Dinotaurent <danieldazawow@gmail.com>
2025-12-29 16:09:46 -0500

    dockerizacion exitosa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import solidJs from '@astrojs/solid-js';
-
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
-import netlify from '@astrojs/netlify';
-
-// https://astro.build/config
 export default defineConfig({
+  output: 'server', //  Obligatorio para SSR
   integrations: [solidJs()],
-
   vite: {
     plugins: [tailwindcss()]
   },
-
-  adapter: netlify()
+  adapter: node({
+    mode: 'standalone' // Genera el servidor para el contenedor
+  })
 });
